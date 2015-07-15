fe7516c6025c58b3d7fb5b3db9ad2d764154b97f
HydraNL <rijkmercuur@gmail.com>
2015-07-15 17:10:49 +1000

    Location GUI seems to work. Sometimes random error pops up saying 'x-already exist' has soomething do with not cleaning up properly after a run?

--- meatSim/src/meatEating/myLocationStyle.java
+++ meatSim/src/meatEating/myLocationStyle.java
@@ -5,11 +5,16 @@ import java.awt.Color;
 import framework.Agent;
 import framework.Location;
 import repast.simphony.visualizationOGL2D.DefaultStyleOGL2D;
+import saf.v3d.ShapeFactory2D;
+import saf.v3d.scene.VSpatial;
 
 public class myLocationStyle extends DefaultStyleOGL2D {
-
 		
-
+		@Override
+		public VSpatial getVSpatial(Object agent, VSpatial spatial) {
+		    return shapeFactory.createRectangle(50, 50);
+		  }
+		
 		@Override
 		public Color getColor(Object o) {
 			Location l = (Location)o;
